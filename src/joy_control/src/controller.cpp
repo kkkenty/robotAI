7322c059637558c1f40278ffec73e1e8dcc604d2
kento <kento@example>
2022-09-22 23:32:36 +0900

    control_pwm_one was maked

--- src/joy_control/src/controller.cpp
+++ src/joy_control/src/controller.cpp
@@ -2,29 +2,42 @@
 // 左右のstickで1ユニットのモータを動かす
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
-#include <msgs/3Steer.h>
+#include <msgs/Steer.h>
 
-msgs::3Steer PWM;
-const float MAX_L = 255.0, MAX_R = 255.0;
+msgs::Steer PWM;
+float MAX_Drive = 255.0, MAX_Steer = 255.0;
 int FRIQUENCE = 100;
-int unit = 0;
+int unit = 2;
 int i;
 
 void joy_callback(const sensor_msgs::Joy &joy_msg)
 {
+  // 初期化
+  PWM.DriveTen = 0; PWM.SteerTen = 0;
+  PWM.DriveSix = 0; PWM.SteerSix = 0;
+  PWM.DriveTwo = 0; PWM.SteerTwo = 0;
   for(i=1;i<5;i++){
-      if(joy_msg.button[i] == 1){
-          unit = i;
+      if(joy_msg.buttons[i-1] == 1){
+          unit = i; // ここでunitの値が変わらない限り、PWMは変わらない
           break;
       }
   }
-  PWM.left  = MAX_L * joy_msg.axes[1];
-  PWM.right = MAX_R * joy_msg.axes[2];
-  if(joy_msg.buttons[0]){
-    PWM.left = 0.0; PWM.right = 0.0;
+  switch(unit){
+    case 1:
+      PWM.DriveTen = MAX_Drive * joy_msg.axes[1];
+      PWM.SteerTen = MAX_Steer * joy_msg.axes[2];
+      break;
+    case 3:
+      PWM.DriveSix = MAX_Drive * joy_msg.axes[1];
+      PWM.SteerSix = MAX_Steer * joy_msg.axes[2];
+      break;
+    case 4:
+      PWM.DriveTwo = MAX_Drive * joy_msg.axes[1];
+      PWM.SteerTwo = MAX_Steer * joy_msg.axes[2];
+      break;
+    default:
+      break;
   }
-  //ROS_INFO("PWM.L:%lf", (int)PWM.L);
-  //ROS_INFO("PWM.R:%lf", (int)PWM.R);
 }
 
 int main(int argc, char **argv)
@@ -32,11 +45,11 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "controller");
   ros::NodeHandle nh;
   ros::NodeHandle pnh("~");
-  pnh.getParam("MAX_L", MAX_L);
-  pnh.getParam("MAX_R", MAX_R);
+  pnh.getParam("MAX_Drive", MAX_Drive);
+  pnh.getParam("MAX_Steer", MAX_Steer);
   pnh.getParam("FRIQUENCE", FRIQUENCE);
   ros::Subscriber sub = nh.subscribe("joy", 10, joy_callback);
-  ros::Publisher pub = nh.advertise<msgs::Motor>("power", 1);
+  ros::Publisher pub = nh.advertise<msgs::Steer>("pwm", 10);
   ros::Rate loop_rate(FRIQUENCE);
 
   while (ros::ok())
@@ -45,5 +58,6 @@ int main(int argc, char **argv)
     ros::spinOnce();
     loop_rate.sleep();
   }
+
   return 0;
 }
